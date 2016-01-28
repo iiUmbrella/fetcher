326d033b2931e49a206cf7a55d7b41e9fc8328b1
Difrex <difrex.punk@gmail.com>
2016-01-28 17:14:07 +0300

    list.txt support

--- II/Config.pm
+++ II/Config.pm
@@ -1,6 +1,9 @@
 package II::Config;
 
 use Config::Tiny;
+use LWP::UserAgent;
+use HTTP::Request;
+use Data::Dumper;
 
 sub new {
     my $class = shift;
@@ -18,12 +21,16 @@ sub load {
     
     my $tiny = Config::Tiny->new();
     $config = $tiny->read($file);
-    
+
     my $key       = $config->{auth}->{key};
     my $nick      = $config->{auth}->{nick};
     my $host      = $config->{node}->{host};
     my @echoareas = split /,/, $config->{node}->{echoareas};
     my $name      = $config->{node}->{name};
+
+    if ( $config->{node}->{list} eq 'yes' ) {
+        @echoareas = echo_from_list($host);
+    }
 
     $c = {
         nick      => $nick,
@@ -36,6 +43,30 @@ sub load {
     };
 
     return $c;
+}
+
+# Make echoareas list from list.txt
+sub echo_from_list {
+    my ($host) = @_;
+
+    my $list = $host . 'list.txt';
+    my @echoes;
+
+    my $ua = LWP::UserAgent->new();
+    $ua->agent("Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0)");
+
+    my $req = HTTP::Request->new( GET => $list );
+    my $res = $ua->request($req);
+
+    if ( $res->is_success ) {
+        my @e = split /\n/, $res->content();
+        while (<@e>) {
+            my @description = split /:/, $_;
+            push @echoes, $description[0];
+        }
+    }
+
+    return @echoes;
 }
 
 1;
